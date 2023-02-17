5ea4a7477d23b553dc193b83ee0e13297b1b9166
Mishkin Evgeny <mjos1995@gmail.com>
2023-02-17 17:08:21 +0400

    added writer

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from docutils.core import publish_file
 
-publish_file(source_path='README3.rst', destination_path='README3.html', parser_name='rst')
+publish_file(
+    source_path="README3.rst",
+    destination_path="README3.html",
+    parser_name="rst",
+    writer_name="html",
+)
